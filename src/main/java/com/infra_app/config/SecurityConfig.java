4eb88e4701b3beea05022f2ee138aa6d141e8d4b
Pushpendra Kushwaha <pk1592098@gmail.com>
2025-06-25 14:15:16 +0530

    implemented swagger

--- src/main/java/com/infra_app/config/SecurityConfig.java
+++ src/main/java/com/infra_app/config/SecurityConfig.java
@@ -1,7 +1,8 @@
 package com.infra_app.config;
 
 import com.infra_app.security.JwtAuthFilter;
-import com.infra_app.util.JwtUtil;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,10 +22,26 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(
+                        "/api/auth/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Infra-app-service")
+                        .version("1.0")
+                        .description("This service is handling Zone, District, Gp, Location, Mandal Api"));
     }
 }
